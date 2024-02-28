bddc05692201c643f6e1b613b96fa0517f18a8e4
Paulo Conde <pauloconde@gmail.com>
2024-02-28 16:27:48 -0400

    Initial Commit

---
+++ src/components/Testimonio.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "../styles/Testimonio.css";
+
+function Testimonio(props) {
+  return (
+    <div className="contenedor-testimonio">
+      <img
+        className="imagen-testimonio"
+        src={require(`../images/testimonio-${ props.imagen }.png`)}
+        alt={`Foto de ${ props.nombre }`}
+      />
+      <div className="contenedor-texto-testimonio">
+        <p className="nombre-testimonio">
+          {props.nombre} en <strong>{props.pais}</strong>
+        </p>
+        <p className="cargo-testimonio">
+          {props.cargo} en <strong>{props.empresa}</strong>
+        </p>
+        <p className="texto-testimonio">{`"${props.testimonio}"`}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Testimonio;
