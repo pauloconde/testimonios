1ba62817a246700f162ee4b3d695bc4f0d5978d0
Paulo Conde <pauloconde@gmail.com>
2024-02-29 11:41:46 -0400

    Use of dangerouslySetInnerHTML

--- src/components/Testimonio.jsx
+++ src/components/Testimonio.jsx
@@ -16,7 +16,7 @@ function Testimonio(props) {
         <p className="cargo-testimonio">
           {props.cargo} en <strong>{props.empresa}</strong>
         </p>
-        <p className="texto-testimonio">{`"${props.testimonio}"`}</p>
+        <p className="texto-testimonio" dangerouslySetInnerHTML={{ __html: props.testimonio} }/>
       </div>
     </div>
   );
