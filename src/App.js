0c867f7cf4a23a46976cdbaf5e9e132d8e185e54
Paulo Conde <pauloconde@gmail.com>
2024-02-29 13:08:37 -0400

    Moving data to JSON

--- src/App.js
+++ src/App.js
@@ -1,35 +1,28 @@
 import "./App.css";
 import Testimonio from "./components/Testimonio";
 
+const TESTIMONIOS = require("./data/testimonios.json");
+console.log(TESTIMONIOS)
+const testimonios = TESTIMONIOS.testimonios;
+
 function App() {
   return (
     <div className="App">
       <div className="contenedor-principal">
         <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
-        <Testimonio 
-        nombre='Shawn Wang'
-        pais='Singapur'
-        imagen='shawn'
-        cargo='Ingeniero de Software'
-        empresa='Amazon'
-        testimonio={'<b>Da miedo cambiar de carrera</b>. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida.'} 
-        />
-        <Testimonio 
-        nombre='Sarah Chima'
-        pais='Nigeria'
-        imagen='sarah'
-        cargo='Ingeniero de Software'
-        empresa='ChatDesk'
-        testimonio='FreeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mis conocimientos de programación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble.' 
-        />
-        <Testimonio 
-        nombre='Emma Bostian'
-        pais='Suecia'
-        imagen='emma'
-        cargo='Ingeniero de Software'
-        empresa='Spotify'
-        testimonio='Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.' 
-        />
+        {testimonios.map((testimonio, index) => {
+          return (
+            <Testimonio
+              key={index}
+              nombre={testimonio.nombre}
+              pais={testimonio.pais}
+              imagen={testimonio.imagen}
+              cargo={testimonio.cargo}
+              empresa={testimonio.empresa}
+              testimonio={testimonio.testimonio}
+            />
+          );
+        })}
       </div>
     </div>
   );
